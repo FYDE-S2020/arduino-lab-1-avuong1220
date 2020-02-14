5a13604ad5a3e51c537164ecb2e078ad9330e107
avuong1220 <60492287+avuong1220@users.noreply.github.com>
2020-02-14 13:47:48 -0600

    Update Lab1_Blink.ino

--- Lab1_Blink/Lab1_Blink.ino
+++ Lab1_Blink/Lab1_Blink.ino
@@ -24,17 +24,41 @@ void setup() {
   pinMode(LED_PIN, OUTPUT);
 }
 
+void dimmer(int freq, int duty) {
+  int period, onTime, offTime;
+  period = 1000/freq;
+  onTime = period * duty / 100;
+  offTime = period - onTime;
+  digitalWrite(LED_PIN, HIGH);
+  delay(onTime);
+  digitalWrite(LED_PIN, LOW);
+  delay(offTime);
+}
+
 void timedBlink(int interval) {
   digitalWrite(LED_PIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(interval);                       // wait for a second
+  delay(125);                       // wait for a second
   digitalWrite(LED_PIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(inverval);                       // wait for a second
+  delay(125);                       // wait for a second
+  digitalWrite(LED_PIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(250);                       // wait for a second
+  digitalWrite(LED_PIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(250); 
+  digitalWrite(LED_PIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(500);                       // wait for a second
+  digitalWrite(LED_PIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(500); 
 }
 
 
 // the loop function runs over and over again forever
 void loop() {
-  timedBlink(125);
-  timedBlink(250);
-  timedBlink(500);
+  for (int i =0; i<100; i++)
+  {
+    dimmer (100, i);
+  }
+  for (int i =100; i>0; i--)
+  {
+    dimmer (100, i);
+  }
 }
